b8d746f59ca3c6788145d14595eae06dddc47aae
Klaus Ma <klausm@nvidia.com>
2024-01-07 08:45:30 +0800

    update ddp
    Signed-off-by: Klaus Ma <klausm@nvidia.com>

--- examples/pytorch/distributed_training.py
+++ examples/pytorch/distributed_training.py
@@ -9,38 +9,84 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
+import os
+import sys
+import tempfile
+from urllib.parse import urlparse
 
-""" Dataset partitioning helper """
-class Partition(object):
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.optim as optim
 
-    def __init__(self, data, index):
-        self.data = data
-        self.index = index
-
-    def __len__(self):
-        return len(self.index)
-
-    def __getitem__(self, index):
-        data_idx = self.index[index]
-        return self.data[data_idx]
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 
-class DataPartitioner(object):
+class ToyModel(nn.Module):
+    def __init__(self):
+        super(ToyModel, self).__init__()
+        self.net1 = nn.Linear(10, 10)
+        self.relu = nn.ReLU()
+        self.net2 = nn.Linear(10, 5)
 
-    def __init__(self, data, sizes=[0.7, 0.2, 0.1], seed=1234):
-        self.data = data
-        self.partitions = []
-        rng = Random()
-        rng.seed(seed)
-        data_len = len(data)
-        indexes = [x for x in range(0, data_len)]
-        rng.shuffle(indexes)
+    def forward(self, x):
+        return self.net2(self.relu(self.net1(x)))
 
-        for frac in sizes:
-            part_len = int(frac * data_len)
-            self.partitions.append(indexes[0:part_len])
-            indexes = indexes[part_len:]
 
-    def use(self, partition):
-        return Partition(self.data, self.partitions[partition])
+def demo_basic(local_world_size, local_rank):
 
+    # setup devices for this process. For local_world_size = 2, num_gpus = 8,
+    # rank 0 uses GPUs [0, 1, 2, 3] and
+    # rank 1 uses GPUs [4, 5, 6, 7].
+    n = torch.cuda.device_count() // local_world_size
+    device_ids = list(range(local_rank * n, (local_rank + 1) * n))
+
+    print(
+        f"[{os.getpid()}] rank = {dist.get_rank()}, "
+        + f"world_size = {dist.get_world_size()}, n = {n}, device_ids = {device_ids} \n", end=''
+    )
+
+    model = ToyModel().cuda(device_ids[0])
+    ddp_model = DDP(model, device_ids)
+
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(device_ids[0])
+    loss_fn(outputs, labels).backward()
+    optimizer.step()
+
+
+def spmd_main(local_world_size, local_rank):
+    # These are the parameters used to initialize the process group
+    env_dict = {
+        key: os.environ[key]
+        for key in ("MASTER_ADDR", "MASTER_PORT", "RANK", "WORLD_SIZE")
+    }
+
+    print(f"[{os.getpid()}] Initializing process group with: {env_dict}")
+    dist.init_process_group(backend="nccl")
+
+    print(
+        f"[{os.getpid()}]: world_size = {dist.get_world_size()}, "
+        + f"rank = {dist.get_rank()}, backend={dist.get_backend()} \n", end=''
+    )
+
+    demo_basic(local_world_size, local_rank)
+
+    # Tear down the process group
+    dist.destroy_process_group()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # This is passed in via launch.py
+    parser.add_argument("--local_rank", type=int, default=0)
+    # This needs to be explicitly passed in
+    parser.add_argument("--local_world_size", type=int, default=1)
+    args = parser.parse_args()
+    # The main entry point is called directly without using subprocess
+    spmd_main(args.local_world_size, args.local_rank)
