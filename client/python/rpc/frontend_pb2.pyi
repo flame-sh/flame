7baa37e0fab31e33c7c3deaa9262334d46732db4
Klaus Ma <klausm@nvidia.com>
2024-02-18 13:34:23 +0000

    init.
    Signed-off-by: Klaus Ma <klausm@nvidia.com>

--- client/python/rpc/frontend_pb2.pyi
+++ client/python/rpc/frontend_pb2.pyi
@@ -5,66 +5,66 @@ from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Opti
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
-class CloseSessionRequest(_message.Message):
-    __slots__ = ["session_id"]
-    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
-    session_id: str
-    def __init__(self, session_id: _Optional[str] = ...) -> None: ...
-
 class CreateSessionRequest(_message.Message):
-    __slots__ = ["session"]
+    __slots__ = ("session",)
     SESSION_FIELD_NUMBER: _ClassVar[int]
     session: _types_pb2.SessionSpec
     def __init__(self, session: _Optional[_Union[_types_pb2.SessionSpec, _Mapping]] = ...) -> None: ...
 
+class DeleteSessionRequest(_message.Message):
+    __slots__ = ("session_id",)
+    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
+    session_id: str
+    def __init__(self, session_id: _Optional[str] = ...) -> None: ...
+
+class OpenSessionRequest(_message.Message):
+    __slots__ = ("session_id",)
+    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
+    session_id: str
+    def __init__(self, session_id: _Optional[str] = ...) -> None: ...
+
+class CloseSessionRequest(_message.Message):
+    __slots__ = ("session_id",)
+    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
+    session_id: str
+    def __init__(self, session_id: _Optional[str] = ...) -> None: ...
+
+class GetSessionRequest(_message.Message):
+    __slots__ = ("session_id",)
+    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
+    session_id: str
+    def __init__(self, session_id: _Optional[str] = ...) -> None: ...
+
+class ListSessionRequest(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
 class CreateTaskRequest(_message.Message):
-    __slots__ = ["task"]
+    __slots__ = ("task",)
     TASK_FIELD_NUMBER: _ClassVar[int]
     task: _types_pb2.TaskSpec
     def __init__(self, task: _Optional[_Union[_types_pb2.TaskSpec, _Mapping]] = ...) -> None: ...
 
-class DeleteSessionRequest(_message.Message):
-    __slots__ = ["session_id"]
-    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
-    session_id: str
-    def __init__(self, session_id: _Optional[str] = ...) -> None: ...
-
 class DeleteTaskRequest(_message.Message):
-    __slots__ = ["session_id", "task_id"]
-    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ("task_id", "session_id")
     TASK_ID_FIELD_NUMBER: _ClassVar[int]
-    session_id: str
-    task_id: str
-    def __init__(self, task_id: _Optional[str] = ..., session_id: _Optional[str] = ...) -> None: ...
-
-class GetSessionRequest(_message.Message):
-    __slots__ = ["session_id"]
     SESSION_ID_FIELD_NUMBER: _ClassVar[int]
+    task_id: str
     session_id: str
-    def __init__(self, session_id: _Optional[str] = ...) -> None: ...
+    def __init__(self, task_id: _Optional[str] = ..., session_id: _Optional[str] = ...) -> None: ...
 
 class GetTaskRequest(_message.Message):
-    __slots__ = ["session_id", "task_id"]
-    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ("task_id", "session_id")
     TASK_ID_FIELD_NUMBER: _ClassVar[int]
-    session_id: str
+    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
     task_id: str
+    session_id: str
     def __init__(self, task_id: _Optional[str] = ..., session_id: _Optional[str] = ...) -> None: ...
 
-class ListSessionRequest(_message.Message):
-    __slots__ = []
-    def __init__(self) -> None: ...
-
-class OpenSessionRequest(_message.Message):
-    __slots__ = ["session_id"]
-    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
-    session_id: str
-    def __init__(self, session_id: _Optional[str] = ...) -> None: ...
-
 class WatchTaskRequest(_message.Message):
-    __slots__ = ["session_id", "task_id"]
-    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ("task_id", "session_id")
     TASK_ID_FIELD_NUMBER: _ClassVar[int]
-    session_id: str
+    SESSION_ID_FIELD_NUMBER: _ClassVar[int]
     task_id: str
+    session_id: str
     def __init__(self, task_id: _Optional[str] = ..., session_id: _Optional[str] = ...) -> None: ...
