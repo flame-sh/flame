6d442606b6225f2904da5eb15f09678c95a1a574
Klaus Ma <klausm@nvidia.com>
2025-02-16 15:25:59 +0200

    Add import dir
    Signed-off-by: Klaus Ma <klausm@nvidia.com>

--- sdk/python/flame/__init__.py
+++ sdk/python/flame/__init__.py
@@ -1,4 +1,12 @@
-# __all__ = ["client", "service", "types_pb2", "types_pb2_grpc", "frontend_pb2", "frontend_pb2_grpc", "shim_pb2", "shim_pb2_grpc"]
+# TODO(k82cn): It's better to re-org the python sdk with rpc subdir.
 
-__all__ = ["client", "service"]
-# __path__ = ["."]
+import os
+import sys
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.append(os.path.join(script_dir, "."))
+
+__all__ = [
+    "client",
+    "service"
+]
