c3b0e6314ac7fd58e945115276f945c952c69ac3
Klaus Ma <klausm@nvidia.com>
2025-08-09 10:12:53 +0300

    fix: Rust SDK unix socket path.
    Signed-off-by: Klaus Ma <klausm@nvidia.com>

--- sdk/rust/src/service/mod.rs
+++ sdk/rust/src/service/mod.rs
@@ -106,7 +106,7 @@ pub async fn run(service: impl FlameService) -> Result<(), Box<dyn std::error::E
 
     let service_id = process::id().to_string();
 
-    let uds = UnixListener::bind(format!("/tmp/flame/shim/{service_id}.sock"))?;
+    let uds = UnixListener::bind(format!("/tmp/flame/shim/fsi.sock"))?;
     let uds_stream = UnixListenerStream::new(uds);
 
     Server::builder()
